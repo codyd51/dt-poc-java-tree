90ab9ffa7dd720c548fe88daa0446e2f1117bcb0
Phillip Tennen <pt@datatheorem.io>
2021-08-13 21:58:01 +0200

    Add another vulnerability

--- app/src/main/java/com/phillipt/zipperdown/App.java
+++ app/src/main/java/com/phillipt/zipperdown/App.java
@@ -11,6 +11,27 @@ public class App {
 	}
 
 	private static void unzip() throws IOException {
+		/*
+		ZipInputStream zis = new ZipInputStream(new FileInputStream("./poc.zip"));
+		ZipEntry ent = zis.getNextEntry();
+		while (ent != null) {
+			String name = ent.getName();
+			System.out.println("Name: " + name);
+			FileOutputStream fos = new FileOutputStream(name);
+			byte[] bytes = new byte[128];
+			int cnt = zis.read(bytes);
+			while (cnt != -1) {
+				fos.write(bytes, 0, cnt);
+				cnt = zis.read(bytes);
+			}
+			ent = zis.getNextEntry();
+		}
+		zis.closeEntry();
+		zis.close();
+		*/
+	}
+
+	private static void unzip_new() throws IOException {
 		ZipInputStream zis = new ZipInputStream(new FileInputStream("./poc.zip"));
 		ZipEntry ent = zis.getNextEntry();
 		while (ent != null) {
