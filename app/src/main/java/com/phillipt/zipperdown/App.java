7b92bac09b3fa5e54d8dbf2eb8863c34e74946ec
Phillip Tennen <pt@datatheorem.io>
2021-09-01 20:34:40 +0200

    Add some hard-coded secrets

--- app/src/main/java/com/phillipt/zipperdown/App.java
+++ app/src/main/java/com/phillipt/zipperdown/App.java
@@ -3,14 +3,31 @@ package com.phillipt.zipperdown;
 import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
-import com.phillipt.zipperdown.KeyStore;
 import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) throws IOException {
 		unzip_new();
 		staticKey();
+
+		// 02-0108 - Google Play Warning: Embedded AWS Credentials"
+		System.out.println("Our AWS credential is " + "AKIAIOSFODNN7EXAMPLE");
+
+		// 02-0123 - Google Play Security Warning: Hardcoded Google Oauth Refresh Token
+		String oauthRefreshToken = "1/DGXUA3FmzHZnjEJDvX8ynqJCALJpo6ZT3IVNK6aBYHU";
+		String refreshToken2 = "1/EeE4LhI8EVRj3F98wmlwFDFnIycu16EY2Gv8dDujT24";
+		System.out.printf("OAuth refresh token: %s token2 %s%n", oauthRefreshToken, refreshToken2);
+
+		// 00-0080 - Hardcoded Kony Password
+		System.out.println("Kony@1234DXir4mzKhY7SuAVoTrjti7Sm is very sensitive");
+
+		// 02-0085 - Sensitive Data Exposed via Ordered Broadcast
+		/*
+		String signaturePermission = "com.app.custom_permission.broadcastpermission";
+		Intent intent = new Intent("Action name");
+		intent.putExtra("PARAM", "Sensitive Info from Sender");
+		sendOrderedBroadcast(intent, signaturePermission, null, null, 0, null, null);
+		*/
 	}
 
 	private static void unzip_new() throws IOException {
@@ -27,6 +44,7 @@ public class App {
 				cnt = zis.read(bytes);
 			}
 			ent = zis.getNextEntry();
+			fos.close();
 		}
 		zis.closeEntry();
 		zis.close();
