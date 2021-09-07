b1554b7d0e92529885a97c332fc83b7b1f14fa16
Phillip Tennen <pt@datatheorem.io>
2021-09-07 13:15:21 +0200

    Remove undefined function call

--- app/src/main/java/com/phillipt/zipperdown/App.java
+++ app/src/main/java/com/phillipt/zipperdown/App.java
@@ -7,7 +7,6 @@ import java.util.zip.ZipOutputStream;
 
 public class App {
 	public static void main(String[] args) throws IOException {
-		abcd();
 		unzip();
 
 		// 02-0108 - Google Play Warning: Embedded AWS Credentials"
